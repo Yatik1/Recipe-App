6cc7f958419b1933356199d8b77cba68bac2ea4f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-07-20 23:57:02 +0530

    :: development -->Cards-sliding completed

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
+import {Splide,SplideSlide} from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css' 
 
 function Popular() {
   
@@ -20,15 +22,26 @@ function Popular() {
 
   return (
     <Wrapper>
-          <h3>Popular Recipes</h3>
-          {popular.map((recipe) => {
-            return (
-             <Card>
-              <p>{recipe.title}</p>
-              <img src={recipe.image} alt='' />
-             </Card>
-            )
-          })}
+        <h3>Popular Recipes</h3>
+         <Splide options={{
+            perPage: 4 ,
+            arrows : false ,
+            pagination: false ,
+            drag: 'free',
+            gap: "3rem"
+         }}>
+             {popular.map((recipe) => {
+               return (
+                <SplideSlide>
+                   <Card key={recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt='' />
+                    <Gradient /> 
+                   </Card>
+                </SplideSlide>
+               )
+             })}
+         </Splide>
     </Wrapper>
   )
 }
@@ -38,8 +51,43 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
-   min-height:25rem;
+   min-height: 20rem;
    border-radius: 2rem;
+   overflow: hidden;
+   position : relative;
+
+   img{
+    border-radius:2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+   }
+   p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50% , 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: bolder;
+    font-size: 1.2rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   }
 `;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5)) ;
+`
 
 export default Popular
