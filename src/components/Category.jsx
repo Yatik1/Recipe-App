ca12a5de43ba22a05afedbcfb5e8ee91f8970068
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-07-26 12:49:30 +0530

    :: routing pages implemented

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,31 +5,78 @@ import styled from 'styled-components'
 
 function Category() {
     return(
-        <Link>
-            <NavLink to={'/cuisine/Italian'}>
+        <List>
+            
+            {/* <>
+              <NavLink>
+                <FaHome to="/"/>
+                 <h4>Home</h4>
+              </NavLink>
+            </> */}
+            
+            <SLink to={'/cuisine/Italian'}>
                 <FaPizzaSlice />
-                <h4>Italian</h4>
-            </NavLink>
-            <NavLink to={'/cuisine/American'}>
+                <h4>Italian</h4> 
+            </SLink>
+
+            <SLink to={'/cuisine/American'}>
                 <FaHamburger />
                 <h4>American</h4>
-            </NavLink>
-            <NavLink>
-                <GiNoodles to={'/cuisine/Thai'}/>
+            </SLink>
+            
+            <SLink to={'/cuisine/Japanese'}>
+                <GiChopsticks />
+                <h4>Japanese</h4>
+            </SLink>
+            
+            <SLink to={'/cuisine/Thai'}>
+                <GiNoodles />
                 <h4>Thai</h4>
-            </NavLink>
-            <NavLink>
-                <GiChopsticks to={'/cuisine/Chineese'}/>
-                <h4>Chineese</h4>
-            </NavLink>
-        </Link>
+            </SLink>
+
+        </List>
     )
 }
 
-const Link = styled.div`
+const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   border-radius: 50%;
+   margin-right:2rem;
+   text-decoration: none;
+   background: linear-gradient(35deg , #494949 , #313131);
+   width: 6rem;
+   height: 6rem;
+   cursor: pointer;
+   transform: scale(0.8); 
+
+   h4{
+    color: white;
+    font-size:0.8rem;
+   }
+   svg{
+    color: white;
+    font-size: 1.5rem;
+   }
+   
+   &:hover ,  &.active {
+    background: linear-gradient(to right , #f27121 , #e94057);
+
+    svg {
+        color: white;
+    }
+    h4{
+        color: white;
+    }
+   }
 `
 
 export default Category
